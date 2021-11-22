4c16bc0500ebae5400aed9cbd1ecde1860b0f576
Hanrea <hanrea@qq.com>
2021-11-22 19:45:10 +0800

    update vue-ssr examples

--- examples/vue-ssr/vite.config.ts
+++ examples/vue-ssr/vite.config.ts
@@ -2,14 +2,17 @@ import { defineConfig } from "vite";
 import VuePlugin from "@vitejs/plugin-vue";
 import Auto from "vite-plugin-autorouter";
 import Restart from "vite-plugin-restart";
-
+import Markdown from "vite-plugin-md";
 const config = defineConfig({
     plugins: [
-        VuePlugin(),
-        Auto(),
+        VuePlugin({ include: [/\.vue$/, /\.md$/],}),
+        Auto({
+            extensions: ["vue", "md"],
+        }),
         Restart({
             restart: ["../../dist/*.js"],
         }),
+        Markdown()
     ],
     build: {
         minify: false,
